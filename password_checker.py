ea8b18a666a266115e533ea8b4066160256aaa13
joespelta <joe.spelta@my.jcu.edu.au>
2021-08-22 11:43:08 +1000

    Add password check program

--- password_checker.py
+++
@@ -1,7 +0,0 @@
-minimum_length = 6
-
-password = input("Enter Password: ")
-while len(password) < minimum_length:
-    print("Invalid Password")
-    password = input("Enter Password: ")
-print(len(password) * "*")
